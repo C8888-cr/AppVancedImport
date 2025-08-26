8a6709d1d52f380488c5ce863738eef0c8374c9b
C8888-cr <christiane100385@web.de>
2025-08-26 16:54:24 +0200

    implementation: Version: koin = "4.1.0", Library: koin-core = { module = "io.insert-koin:koin-core", version.ref = "koin" } shared/commonMain.dependencies:  implementation(libs.koin.core)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+
 }
 
 kotlin {
@@ -26,8 +27,9 @@ kotlin {
     }
     
     sourceSets {
+
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
+            implementation(libs.koin.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
