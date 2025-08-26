674b64fc4469334013920528dbb74993ea6bf866
C8888-cr <christiane100385@web.de>
2025-08-26 17:28:33 +0200

    changed MainActivity.kt to call the suspending greeting function

--- composeApp/src/androidMain/kotlin/com/example/appvancedimport/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/appvancedimport/MainActivity.kt
@@ -1,25 +1,58 @@
-package com.example.appvancedimport
+package com.example.kmmktor.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.kmmktor.Greeting
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
         setContent {
-            App()
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val scope = rememberCoroutineScope()
+                    var text by remember { mutableStateOf("Loading") }
+                    LaunchedEffect(true) {
+                        scope.launch {
+                            text = try {
+                                Greeting().greeting()
+                            } catch (e: Exception) {
+                                e.localizedMessage ?: "error"
+                            }
+                        }
+                    }
+                    GreetingView(text)
+                }
+            }
         }
     }
 }
 
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
+}
+
 @Preview
 @Composable
-fun AppAndroidPreview() {
-    App()
+fun DefaultPreview() {
+    MyApplicationTheme {
+        GreetingView("Hello, Android!")
+    }
 }
